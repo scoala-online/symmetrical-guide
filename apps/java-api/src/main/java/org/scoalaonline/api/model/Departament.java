00216b3282c12d122b519c684555e0010e14b49a
Dennis Curti <denniscurti@yahoo.com>
2021-03-21 16:38:47 +0200

    feat: 🎸 Add department class
    ✅ Closes: 5

--- apps/java-api/src/main/java/org/scoalaonline/api/model/Departament.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/model/Departament.java
@@ -1,4 +1,29 @@
 package org.scoalaonline.api.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "department")
 public class Departament {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "department_id")
+  private long id;
+
+  @Column(name = "department_name", nullable = false, length = 20)
+  private String departamentName
+
+  public Departament() {
+  }
+
+  public long getId() { return id; }
+
+  public String getDepartamentName() { return departamentName; }
+
+  public void setId(long id) { this.id = id; }
+
+  public
+
 }
